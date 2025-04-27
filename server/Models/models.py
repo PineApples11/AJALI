1953550a46f3df8cf9fd4f30a3b7999b3acad74e
PineApples11 <kinyanjuiivy1@gmail.com>
2025-04-27 12:06:27 +0300

    seeding and creation of  random dummy data

--- server/Models/models.py
+++ server/Models/models.py
@@ -51,7 +51,7 @@ class User(db.Model,SerializerMixin):
     username = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(120), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    phone_number = db.Column(db.String(20), nullable=False)
+    phone_number = db.Column(db.String(50), nullable=False)
 
     incident_reports = db.relationship('IncidentReport', backref='user', lazy=True)
    
